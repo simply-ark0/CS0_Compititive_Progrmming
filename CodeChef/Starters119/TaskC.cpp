ff06fec2a04b1589d89aaaa0eedb3e4882f90dfa
Arkojeet Bera <73406774+simply-arko@users.noreply.github.com>
2024-02-21 00:51:54 +0530

    Add files via upload

---
+++ CodeChef/Starters119/TaskC.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vs vector<string>
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long>>
+#define vvll vector<vector<long long>>
+#define pb(a) push_back(a)
+#define umpi unordered_map<int, int>
+#define usi unordered_set<int>
+#define cbug(a) cerr<<a<<endl;
+#define dbug(a) cout<<a<<endl;
+
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}\n"; }
+
+void file_i_o(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main(int argc, char const *argv[]){
+	clock_t begin = clock();
+	file_i_o();
+
+	// write your code here...
+	int t;
+	cin>>t;
+
+	while(t--){
+		int n, q;
+		cin>>n>>q;
+		string str;
+		cin>>str;
+		int mxW = 1, crW = 1;
+		char prevChar = str[0];
+
+		for(int i=1; i<n; i++){
+			if(str[i]==prevChar)
+				++crW;
+			else{
+				prevChar = str[i];
+				crW = 1;
+			}
+			mxW = max(mxW, crW);
+		}
+		cout<<mxW<<" ";
+
+		while(q--){
+			char ch;
+			cin>>ch;
+
+			if(ch==prevChar)
+				++crW;
+			else{
+				prevChar = ch;
+				crW = 1;
+			}
+			mxW = max(mxW, crW);
+			cout<<mxW<<" ";
+		}
+
+		cout<<endl;
+	}
+
+	#ifndef ONLINE_JUDGE
+		clock_t end = clock();
+		cout<<"\n\n Executed in: "<<double(end-begin)/CLOCKS_PER_SEC*1000<<" ms";
+	#endif
+
+	return EXIT_SUCCESS;
+} 
---
+++ CodeChef/Starters119/TaskD.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define lli long long int
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vs vector<string>
+#define vi vector<int>
+#define vll vector<long long>
+#define pb(a) push_back(a)
+#define umpii map<int, int>
+#define mpii map<int, int>
+#define si set<int>
+
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}\n"; }
+
+void file_i_o(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main(int argc, char const *argv[]){
+	clock_t begin = clock();
+	file_i_o();
+
+	// write your code here...
+	int t;
+	cin>>t;
+
+	while(t--){
+		int n, k;
+		cin>>n>>k;
+		for(int i=1; i<=k; i++)
+			cout<<i<<" ";
+		for(int i=k+2; i<=n; i+=2)
+			cout<<i<<" ";
+		for(int i=k+1; i<=n; i+=2)
+			cout<<i<<" ";
+		cout<<endl;
+	}
+
+	#ifndef ONLINE_JUDGE
+		clock_t end = clock();
+		cout<<"\n\n Executed in: "<<double(end-begin)/CLOCKS_PER_SEC*1000<<" ms";
+	#endif
+
+	return EXIT_SUCCESS;
+} 
